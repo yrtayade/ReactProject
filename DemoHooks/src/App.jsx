1b958a54577eab5a5f3a84a079556e60388ea2e9
Yogesh Tayade <yotayade@gmail.com>
2024-06-11 01:18:23 +0530

    timeout

--- DemoHooks/src/App.jsx
+++ DemoHooks/src/App.jsx
@@ -3,27 +3,33 @@ import BlogList from "./BlogList"
 
 function App() {
 
-const [blogs,setBlogs]  = useState(null);
+  const [blogs,setBlogs]  = useState(null);
+  const [isPending,setPending] = useState(true);
 
   function deleteBlog(id)
   {
     const newBlogs= blogs.filter(  (blog)=> blog.id!=id  );
     setBlogs(newBlogs);
   }
-
-  const [name, setName] = useState("Ramesh");
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
+    
+    setTimeout(
+      ()=>
+        fetch("http://localhost:8000/blogs")
       .then(res => res.json())
       .then(data => {
         setBlogs(data);
-      });
+        setPending(false);
+      })
+      
+      ,1000)
+
   }, []);
 
   return (
     <>
-      { blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog}/>}
-      
+      { isPending && <div className="container rounded-5 text-white text-center mt-5 p-5 bg-secondary">Loading..</div>}
+      { blogs && <BlogList blogs={blogs} deleteBlog={deleteBlog}/>}      
     </>
   )
 }
